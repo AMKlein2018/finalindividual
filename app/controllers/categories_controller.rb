bc982415208834e9e188c378d17844d9f8578f82
AMKlein2018 <adamklein0519@gmail.com>
2018-07-01 16:21:22 -0400

    created individual sports links to show up on individual category show pages with blogs, title and articles

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -9,5 +9,15 @@ class CategoriesController < ApplicationController
   end
 
   def show
+  	@category = Category.find(params[:id])
   end
+
+
+
+  private
+  def categories_params
+	params.require(:category).permit(:title, :blog_id, :user_id)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
-  get 'categories/index'
-  get 'categories/new'
-  get 'categories/edit'
-  get 'categories/show'
+
   get "/profile/:id" => "profile#show"
 
   patch "/users/:id", to: "user#update", as: 'edit_profile_favorites'
   devise_for :users
   root 'blogs#index'
   # resources :blogs
+
+  resources :categories
 
   resources :blogs do
     resources :comments
