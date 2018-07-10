546998700641fcd7d844c9eec2f5bb8ca0bd107b
AMKlein2018 <adamklein0519@gmail.com>
2018-07-10 11:52:44 -0400

    fixed favecategory and and added post-container to fit stylize image with content on all different pages

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,6 +2,9 @@ class UserController < ApplicationController
 
   def update
     @user = current_user
+    @user.favorites.each do |favorite|
+      favorite.destroy
+    end
     user_params[:category_ids].each do |id|
     	if id != ""
 		    Favorite.create(user_id: @user.id, category_id: id)
@@ -9,6 +12,11 @@ class UserController < ApplicationController
     end
     redirect_to user_profile_path(@user)
   end
+
+  def destroy
+
+  end
+
 
   protected
 
