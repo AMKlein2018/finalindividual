9d6cbaf581621c3d6a6ae1a0b181eaa2741437b1
AMKlein2018 <adamklein0519@gmail.com>
2018-10-12 15:09:32 -0400

    changes to connection.rb file

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,20 +1,43 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
-  	  identified_by :current_user
+    identified_by :current_user
 
     def connect
       self.current_user = find_verified_user
-      logger.add_tags 'ActionCable', "User #{current_user.id}"
+      logger.add_tags 'ActionCable', current_user.name
     end
 
     protected
-    def find_verified_user
-      if current_user = env['warden'].user
-        current_user
-      else
-        reject_unauthorized_connection
+      def find_verified_user
+        verified_user = User.find_by(id: cookies.signed['user.id'])
+        if verified_user && cookies.signed['user.expires_at'] > Time.now
+          verified_user
+        else
+          reject_unauthorized_connection
+        end
       end
-    end
-    
   end
 end
+
+
+# module ApplicationCable
+#   class Connection < ActionCable::Connection::Base
+#   	  identified_by :current_user
+
+#     def connect
+#       self.current_user = find_verified_user
+#       logger.add_tags 'ActionCable', "User #{current_user.id}"
+#     end
+
+#     protected
+#     def find_verified_user
+#       if current_user = env['warden'].user
+#         current_user
+#       else
+#         reject_unauthorized_connection
+#       end
+#     end
+    
+#   end
+# end
+
